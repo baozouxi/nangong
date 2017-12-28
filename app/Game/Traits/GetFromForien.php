df82a9e675db87e7d7c81b57fe54a9e3e785fa07
sx <sx@sxdeiMac.local>
2017-12-28 18:01:19 +0800

    k

--- app/Game/Traits/GetFromForien.php
+++ app/Game/Traits/GetFromForien.php
@@ -12,13 +12,35 @@ namespace App\Game\Traits;
 trait GetFromForien
 {
 
-
-
     //从国外官网采集
     public function parse($body)
     {
+        $codes_list = json_decode($body, true);
+
+        $codes = array_shift($codes_list);
+
+        $codes['drawNbrs'] = array_sort($codes['drawNbrs']);
+
+        $temp_arr = [];
+        $temp_arr[0] = 0;
+        $temp_arr[1] = 0;
+        $temp_arr[2] = 0;
+        for ($i=0; $i<3; $i++) {
+            $current = $i+1;
+            for ($j=0;$j<=5;$j++) {
+                $temp_arr[$i] += $codes['drawNbrs'][$current];
+                $current += 3;
+            }
+            $temp_arr[$i] = substr((string)$temp_arr[$i], -1, 1);
+        }
 
 
+        $result = [];
+        $result['codes'] = implode(',', $temp_arr);
+        $result['actionNo'] = $codes['drawNbr'];
+        $result['open_time'] = date('Y-m-d H:i:s', time());
+
+        return $result;
     }
 
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,7 +50,28 @@ class HomeController extends Controller
 
         $codes = array_shift($codes_list);
 
-        dd($codes);
+        $codes['drawNbrs'] = array_sort($codes['drawNbrs']);
+
+        $temp_arr = [];
+        $temp_arr[0] = 0;
+        $temp_arr[1] = 0;
+        $temp_arr[2] = 0;
+        for ($i=0; $i<3; $i++) {
+            $current = $i+1;
+            for ($j=0;$j<=5;$j++) {
+                $temp_arr[$i] += $codes['drawNbrs'][$current];
+                $current += 3;
+            }
+            $temp_arr[$i] = substr((string)$temp_arr[$i], -1, 1);
+        }
+
+
+        $result = [];
+        $result['codes'] = implode(',', $temp_arr);
+        $result['actionNo'] = $codes['drawNbr'];
+        $result['open_time'] = date('Y-m-d H:i:s', time());
+
+        return $result;
 
     }
 
